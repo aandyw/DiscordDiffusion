9fbc7090ddeb565820baa2c8c6552bd52844428d
Andy W <andy999coc@gmail.com>
2024-06-19 19:55:04 -0400

    script run no issues

--- apps/diffusion/train/train_sdxl.sh
+++ apps/diffusion/train/train_sdxl.sh
@@ -10,21 +10,19 @@ wandb login
 # set accelerate configurations
 accelerate config default
 
-# lauch 
-accelerate launch train_text_to_image_sdxl.py \
+# train!
+accelerate launch train_text_to_image_lora_sdxl.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --pretrained_vae_model_name_or_path=$VAE_NAME \
-  --dataset_name=$DATASET_NAME \
-  --enable_xformers_memory_efficient_attention \
-  --resolution=512 --center_crop --random_flip \
-  --proportion_empty_prompts=0.2 \
+  --dataset_name=$DATASET_NAME --caption_column="text" \
+  --resolution=1024 --random_flip \
   --train_batch_size=1 \
-  --gradient_accumulation_steps=4 --gradient_checkpointing \
-  --max_train_steps=10000 \
-  --use_8bit_adam \
-  --learning_rate=1e-06 --lr_scheduler="constant" --lr_warmup_steps=0 \
+  --num_train_epochs=2 --checkpointing_steps=500 \
+  --learning_rate=1e-04 --lr_scheduler="constant" --lr_warmup_steps=0 \
   --mixed_precision="fp16" \
-  --report_to="wandb" \
-  --validation_prompt="shrek" --validation_epochs 5 \
-  --checkpointing_steps=5000 \
+  --seed=42 \
   --output_dir="sdxl-ddiff-model" \
+  --validation_prompt="cute dragon creature" \
+  --report_to="wandb" \
+  --push_to_hub
+  
