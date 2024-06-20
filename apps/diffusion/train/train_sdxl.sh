b0dff287b99e151db37389af2096da4e9e055aa2
Andy W <andy999coc@gmail.com>
2024-06-19 22:42:08 -0400

    updagte

--- apps/diffusion/train/train_sdxl.sh
+++ apps/diffusion/train/train_sdxl.sh
@@ -15,9 +15,9 @@ accelerate launch train_text_to_image_lora_sdxl.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --pretrained_vae_model_name_or_path=$VAE_NAME \
   --dataset_name=$DATASET_NAME --caption_column="text" \
-  --resolution=1024 --random_flip \
-  --train_batch_size=1 \
-  --num_train_epochs=2 --checkpointing_steps=500 \
+  --resolution=512 --random_flip \
+  --train_batch_size=2 \
+  --num_train_epochs=30 --checkpointing_steps=500 \
   --learning_rate=1e-04 --lr_scheduler="constant" --lr_warmup_steps=0 \
   --mixed_precision="fp16" \
   --seed=42 \
